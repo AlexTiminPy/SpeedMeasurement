2e9817028a1f24dd59fa77f867556f15fa8a975d
1080176 <AlexTiminPy>
2022-02-19 23:19:31 +0300

    i learn pdoc

---
+++ test_work/test_work_with_pdoc.py
@@ -0,0 +1,9 @@
+def print_message(name: str, surname: str) -> None:
+    """
+    :param name:    is your name
+    :param surname: is your surname
+    """
+    print(f"name: {name} surname: {surname}")
+
+
+print_message("Алексей", "Тимин")
