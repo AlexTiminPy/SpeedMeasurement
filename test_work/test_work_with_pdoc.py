a5720f2149d0e857471bb6cbbfc9aa15f3f3b8d9
1080176 <AlexTiminPy>
2022-02-20 14:25:02 +0300

    i add new function

--- test_work/test_work_with_pdoc.py
+++ test_work/test_work_with_pdoc.py
@@ -1,9 +1,27 @@
-def print_message(name: str, surname: str) -> None:
+from rich import print as rprint
+from rich.console import Console
+from rich.panel import Panel
+
+
+def get_message(name: str, surname: str) -> str:
     """
     :param name:    is your name
     :param surname: is your surname
+    :rtype string
     """
-    print(f"name: {name} surname: {surname}")
+    return f"{name=} {surname=}"
 
 
-print_message("Алексей", "Тимин")
+def get_alter_message(name: str, surname: str) -> str:
+    """
+    :param name:    is your name
+    :param surname: is your surname
+    :rtype string
+    """
+    return f"{surname=} {name=}"
+
+
+console = Console(width=50, stderr=True, style="black on white")
+console.print(get_message("Алексей", "Тимин"))
+console.print(Panel.fit(f"{get_alter_message('Алексей', 'Тимин')}", border_style="red"))
+console.rule("[bold red]Chapter 2")
